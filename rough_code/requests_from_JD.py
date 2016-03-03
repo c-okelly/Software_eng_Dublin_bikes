8c9cdcbdd0d6b8332bc25642e5bfbb61e92ee01e
cokelly <okellyconor@gmail.com>
2016-03-03 11:55:33 +0000

    updated rougher work for data phraser

--- rough_code/requests_from_JD.py
+++ rough_code/requests_from_JD.py
@@ -5,12 +5,27 @@
 
 
 import urllib.request as request
+import re,json
 
-key = 'a4dc19867e72bc955aa9a438f2b90a8c7b6067f7l'
+key_append = '&apiKey=a4dc19867e72bc955aa9a438f2b90a8c7b6067f7'
 
-url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=a4dc19867e72bc955aa9a438f2b90a8c7b6067f7"
+url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin"
+
+specfic_station = "https://api.jcdecaux.com/vls/v1/stations/42?contract=Dublin"
 
 
-file = request.urlopen(url)
+file = request.urlopen(url+key_append).read()
+# object = json.loads('{"number":42,"name":"SMITHFIELD NORTH","address":"Smithfield North","position":{"lat":53.349562,"lng":-6.278198},"banking":true,"bonus":false,"status":"OPEN","contract_name":"Dublin","bike_stands":30,"available_bike_stands":24,"available_bikes":6,"last_update":1457002251000}')
 
-print (file.read())
+
+file_read = file.decode('UTF-8')
+print(file_read.count("number"))
+file = (re.findall('\{.*?\}.*?\}', file_read))
+
+dict = json.loads(file_read)
+
+item = dict[0]
+print(item)
+
+print(item.get("contract_name"))
+
