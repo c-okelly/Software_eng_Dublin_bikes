43013459e39ca017c0b98c165c0ce1dca67a25f7
ShaneKenny <shane.kenny89@gmail.com>
2016-04-04 16:02:09 +0100

    main method adapted

--- master.py
+++ master.py
@@ -1,14 +1,23 @@
 
 _author__ = "Shane Kenny"
 
-import sys
 
-from Call_api_and_save_disk import *
+import urllib
+import Call_api_and_save_disk
+import sqllite_database
+
+
+
 
 def main():
   "main point of entry for project"
+  sqllite_database()
+  #call sql database creation py file.
   Call_api_and_save_disk()
-  pass
+  # call scrape and update file.
 
-if __name__ == "__main__":
-  main()
+
+
+
+if __name__ == '__main__':
+    main()
