e752816e6fbd166ac3f38119fc6ba161e4c8dc26
cokelly <okellyconor@gmail.com>
2016-04-20 21:37:33 +0100

    Changed two function names back to point at Call_api_and_save_disk module

--- master.py
+++ master.py
@@ -9,9 +9,9 @@ def main():
   "main point of entry for project"
 
   print('Creating or opening database')
-  static_data_dict = sqllite_database.return_static_data()
+  static_data_dict = Call_api_and_save_disk.return_static_data()
   sqllite_database.create_database(static_data_dict, 'Historical_data/Data', 'Historical_data/Data_old')
-  dynamic_data_dict = sqllite_database.format_station_data('c22c69d2077c8520674a591abf2aaaccbf6e2440')
+  dynamic_data_dict = Call_api_and_save_disk.format_station_data('c22c69d2077c8520674a591abf2aaaccbf6e2440')
   sqllite_database.import_dynamic_data(dynamic_data_dict)
 
   print("Starting regular scrape of dynamic data now")
