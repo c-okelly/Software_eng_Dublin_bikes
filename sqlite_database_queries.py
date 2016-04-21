bd7a069a0c5d515d4c2bd9a6a7bdff5d61e294af
CFitzmaurice <CFitzmaurice@users.noreply.github.com>
2016-04-21 13:55:12 +0100

    Add function to populate averages' tables

--- sqlite_database_queries.py
+++ sqlite_database_queries.py
@@ -47,3 +47,23 @@ def create_average_tables():
     conn.commit()
     cur.close()
     conn.close()
+    
+def populate_averages():
+    conn = lite.connect('C:/Users/Connor Fitzmaurice/Documents/COMP30670/SoftwareProject/ProjectCode/dublinbikes_database.db')
+    #try:
+    with conn:
+        cur = conn.cursor()
+        # Populate table with Average weekly bikes and stands for each each day for all stations
+        cur.execute("INSERT OR IGNORE INTO Weekly_Averages(Station_number, Weekday,  Average_available_bikes,  Average_Available_bike_stands)"
+                    "SELECT Station_number, Weekday, AVG(Available_Bikes), AVG(Available_bike_stands)"
+                    " FROM Dynamic_Data GROUP BY Station_number, Weekday")
+
+        #  Populate table  Average hourly bikes and stands for all stations for one week
+        cur.execute( "INSERT OR IGNORE INTO Daily_Averages(Station_number, Weekday, Hour, Average_available_bikes, Average_Available_bike_stands)"
+                     "SELECT Station_number, Weekday, (Timestamp % 10000)/100, AVG(Available_bikes), AVG(Available_bike_stands) "
+                     "FROM Dynamic_Data GROUP BY Station_number, Weekday, (Timestamp % 10000)/100")
+    #except:
+     #print("Error in approach")
+    conn.commit()
+    cur.close()
+    conn.close()
