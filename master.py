8746878f904df81fab2c9b894a2590204a13823a
cokelly <okellyconor@gmail.com>
2016-04-22 13:04:44 +0100

    Updated master function to run with new insert_dynamic_data functions

--- master.py
+++ master.py
@@ -3,6 +3,7 @@ _author__ = "Shane Kenny"
 
 import sqllite_database
 import Call_api_and_save_disk
+import insert_dynamic_data
 
 
 def main():
@@ -14,9 +15,9 @@ def main():
   dynamic_data_dict = Call_api_and_save_disk.format_station_data('c22c69d2077c8520674a591abf2aaaccbf6e2440')
   sqllite_database.import_dynamic_data(dynamic_data_dict)
 
+  # New function to run every minute that saves json to file and updates db
   print("Starting regular scrape of dynamic data now")
-  Call_api_and_save_disk.run_every_x_minutes()
-  Call_api_and_save_disk.return_static_data()
+  insert_dynamic_data.run_every_x_minutes_database("dublinbikes_test_database.db")
 
 
 
